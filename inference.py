23149a2581d4f51bb9be75eed9534f79df6d1866
Daniel Valcarce <davamix@gmail.com>
2020-01-04 18:32:33 +0100

    Train function completed with data loader. Old inference code moved to sample_inference.

--- inference.py
+++ inference.py
@@ -1,50 +1,5 @@
-import detectron2
-import numpy as np
-
-#import cv2
-import matplotlib.pyplot as plt
-from PIL import Image
-
-from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog
-from torchvision import transforms
-
-
-# Load image
-#im = cv2.imread("./input.jpg")
-im = Image.open("./input.jpg")
-# Enable to show window images on console
-#cv2.startWindowThread()
-#cv2.namedWindow("preview")
-
-# Convert Image to numpy array expected by the predictor
-im_array = np.array(im)
-print(im_array.shape)
-
+from detectron2.engine import DefaultPredictor
 
 cfg = get_cfg()
-
-# Load model
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
-cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
-
-predictor = DefaultPredictor(cfg)
-outputs = predictor(im_array)
-
-print(outputs["instances"].pred_classes)
-print(outputs["instances"].pred_boxes)
-
-# Visualize boxes
-v = Visualizer(im_array[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
-v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-
-# Save predictions
-fig = plt.figure()
-plt.imshow(v.get_image()[:,:,::-1])
-fig.savefig("./prediction.jpg")
-#cv2.imshow("preview", v.get_image()[:, :, ::-1])
-#cv2.waitKey()
+cfg.MODEL.WEIGHTS(os.path.join(cfg.OUT))
---
+++ train.py
@@ -0,0 +1,24 @@
+import os
+
+from detectron2 import model_zoo
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+
+from loader import get_balloon_dicts
+
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = ("balloon_train",)
+cfg.DATASETS.TEST = ()
+cfg.DATALOADER.NUM_WORKERS = 4
+cfg.MODEL.WEIGTHS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
+cfg.SOLVER.IMS_PER_BATCH = 2
+cfg.SOLVER.BASE_LR = 0.00025
+cfg.SOLVER.MAX_ITER = 300
+cfg.MODEL.ROI_HEADS.BATCH_PER_IMAGE = 128
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg)
+trainer.resume_or_load(resume=False)
+trainer.train()
